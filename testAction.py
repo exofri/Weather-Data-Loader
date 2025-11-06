8aaef4f966336cd1a5b10b2eacd560086c13afd6
exofri <41285243+exofri@users.noreply.github.com>
2025-11-06 10:33:39 +0100

    Enhance update_city function and CSV operations
    Refactor weather update logic and improve file handling.

--- testAction.py
+++ testAction.py
@@ -1,10 +1,97 @@
+#!/usr/bin/python
+
+import csv
+import random
+
+from datetime import date
+import time
 import os
-import datetime
+import subprocess
 
 
-now = datetime.datetime.now()
+def update_city(city):
 
+    t = time.localtime()
+    current_time = time.strftime(" %H:%M:%S", t)
+    today = date.today()
+    # dd/mm/YY
+    today_date = today.strftime('%Y-%m-%d')
+    
+    text_code = subprocess.run(['curl', f"wttr.in/{city}?format=+%t+%h"], stdout=subprocess.PIPE).stdout.decode()
+     # +16°C 94%
+    try:
+        print(int(text_code[text_code.index('°C')-3:text_code.index('°C')]))
+        tmp=int(text_code[text_code.index('°C')-3:text_code.index('°C')])
+    except:
+        print("/!\ error in reading")
+        tmp="Unknown"
+    try:
+        print(int(text_code[text_code.index('%')-2:text_code.index('%')]))
+        humidity=text_code[text_code.index('%')-2:text_code.index('%')]
+    except:
+        humidity="Unknown"
+    
+    
+    if random.random()>0.01:
+        paris_humadity=humidity
+    else:
+        paris_humadity="Unknown"
+    if random.random()>0.015:
+        paris_tmp=tmp
+    else:
+        paris_tmp="Unknown"
+    
+    file=[]
+    # Read CSV file
+    with open(f"{city}.txt") as f:
+        reader = csv.reader(f)
+        headers = next(reader)
+        for row in reader:
+            file.append([r for r in row])
 
-with open('out.txt', 'a+') as file:
-     
-    file.writelines(str(now.year)+"/"+ str(now.month)+"/"+ str(now.day)+"   "+ str(now.hour)+":"+ str(now.minute)+":"+ str(now.second)+"\n")
+    file.append([today_date+current_time,
+                 paris_tmp,
+                 paris_humadity])
+
+    # Write CSV file
+    with open(f"{city}.txt", "wt") as fp:
+        writer = csv.writer(fp, delimiter=",")
+        writer.writerow(i for i in headers)
+        writer.writerows(file[-24*30:])
+
+    return tmp,humidity
+
+# while True:
+    
+#     t = time.localtime()
+#     current_time = time.strftime(" %H:%M:%S", t)
+#     today = date.today()
+#     # dd/mm/YY
+#     today_date = today.strftime('%Y-%m-%d')
+    
+#     for c in ["Paris","Strasbourg","Marseille","Lille"]:
+#         tmp,humidity=update_city(c)
+#         print(f"City:{c}, T:{tmp}, H:{humidity}\n\n")
+    
+#     print(today_date+current_time)
+#     os.system('git commit -a -m "now"')
+#     os.system('git push')
+
+#     time.sleep(3600)
+
+if __name__ == "__main__":
+    t = time.localtime()
+    current_time = time.strftime(" %H:%M:%S", t)
+    today = date.today()
+    # dd/mm/YY
+    today_date = today.strftime('%Y-%m-%d')
+    
+    for c in ["Paris","Strasbourg","Marseille","Lille"]:
+        tmp,humidity=update_city(c)
+        print(f"City:{c}, T:{tmp}, H:{humidity}\n\n")
+    
+    print(today_date+current_time)
+    # git_commit_push()
+
+    os.system('git commit -a -m "now"')
+    os.system('git push')
